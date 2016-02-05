dd7a2db42c1ef3856d55162aeae579a51e51e6fa
Sergey Salnikov <sergeysalnikov@MacBook-Etirec.local>
2016-02-05 17:27:49 +0300

    no message

---
+++ MoviesClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoviesClient
+//
+//  Created by Sergey Salnikov on 05.02.16.
+//  Copyright © 2016 Sergey Salnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
