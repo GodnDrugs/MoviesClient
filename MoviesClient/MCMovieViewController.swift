bc1301f9184de4b42900aca205814b35c351ecd2
Sergey Salnikov <sergeysalnikov@MacBook-Etirec.local>
2016-02-08 12:56:23 +0300

    no message

---
+++ MoviesClient/MCMovieViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MCMovieViewController.swift
+//  
+//
+//  Created by Sergey Salnikov on 06.02.16.
+//
+//
+
+import UIKit
+
+class MCMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MoviesClient/MCSearchViewController.swift
+++ MoviesClient/MCSearchViewController.swift
@@ -8,17 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+class MCSearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
 
