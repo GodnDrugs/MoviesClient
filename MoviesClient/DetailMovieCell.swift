f3ad5469da2cac35db97d153566e13b6af7fc2ec
Sergey Salnikov <sergeysalnikov@MacBook-Etirec.local>
2016-02-24 11:01:40 +0300

    no message

---
+++ MoviesClient/DetailMovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailMovieCell.swift
+//  MoviesClient
+//
+//  Created by Sergey Salnikov on 23.02.16.
+//  Copyright © 2016 Sergey Salnikov. All rights reserved.
+//
+
+import UIKit
+
+class DetailMovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ MoviesClient/ResultFoundMovie.swift
@@ -0,0 +1,13 @@
+//
+//  ResultFoundMovie.swift
+//  MoviesClient
+//
+//  Created by Sergey Salnikov on 23.02.16.
+//  Copyright © 2016 Sergey Salnikov. All rights reserved.
+//
+
+import UIKit
+
+class ResultFoundMovie: NSObject {
+
+}
